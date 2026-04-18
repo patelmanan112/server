7da1e697e4a669ca0bdcc33fb944d5a11b6a0e03
patelmanan112 <manan.patel.cg@gmail.com>
2026-04-18 11:21:06 +0530

    adding dotenv file in server

--- project/src/index.js
+++ project/src/index.js
@@ -1,10 +1,10 @@
+const dotenv = require("dotenv")
 const app = require("./app");
 const ConnectDb = require("./config/db")
-
-
+const port = process.env.PORT || 3000
  function StartServer(){
 ConnectDb()
-app.listen(3000 , ()=>{
+app.listen(port , ()=>{
     console.log("Server has been started on the port 3000");
     
 })
